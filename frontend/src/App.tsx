9edd5443b6779a8aafdb3109ac0ed39be4a6dd86
unknown <sanguec777@gmail.com>
2022-09-06 01:25:39 -0300

    Use State

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import Header from "./componentes/Header"
-import SalesCard from "./componentes/salesCard"
+import Header from "./componentes/Header";
+import SalesCard from "./componentes/salesCard";
 
 function App() {
   return (
@@ -10,4 +10,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
