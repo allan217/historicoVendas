a91519de09ec5e1f1faa8e5acd6876f61ab184da
unknown <sanguec777@gmail.com>
2022-09-10 10:32:28 -0300

    Send notification

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./componentes/Header";
 import SalesCard from "./componentes/salesCard";
 
